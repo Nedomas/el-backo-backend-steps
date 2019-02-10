c64263d6b221bca3e9912b60a7b321518200c558
Domas <domas.bitvinskas@me.com>
2019-02-10 22:49:45 +0200

    2.4.2 - Add createGame mutation related resolvers and others

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { prisma } = require('./prisma/generated/prisma-client')
 
 const typeDefs = gql`
   type Query {
     hello: String
+  }
+
+  type Game {
+    id: ID!
+    players: [Player!]!
+  }
+
+  type Player {
+    id: ID!
+    name: String!
+  }
+
+  type Mutation {
+    createGame(players: [PlayerInput!]!): Game!
+  }
+
+  input PlayerInput {
+    name: String!
   }
 `
 
@@ -10,9 +29,34 @@ const resolvers = {
   Query: {
     hello: () => 'world',
   },
+  Game: {
+    players(root, args, context) {
+      return context.prisma.game({
+        id: root.id
+      }).players()
+    },
+  },
+  Mutation: {
+    createGame(root, args, context) {
+      return context.prisma.createGame(
+        {
+          players: {
+            create: args.players,
+          }
+        },
+
+      )
+    },
+  },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    prisma,
+  },
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
